cfc4bad440772441d6a7653e0643447622ad0d36
vinayak-rajput <vk.ra.org@gmail.com>
2026-08-14 21:39:22 +0530

    Project Structure Defined

---
+++ src/com/hdfclife/Main.java
@@ -0,0 +1,7 @@
+package com.hdfclife;
+
+public class Main {
+    static void main() {
+
+    }
+}
---
+++ src/com/hdfclife/exception/PolicyServiceException.java
@@ -0,0 +1,7 @@
+package com.hdfclife.exception;
+
+public class PolicyServiceException extends RuntimeException {
+    public PolicyServiceException(String message) {
+        super(message);
+    }
+}
