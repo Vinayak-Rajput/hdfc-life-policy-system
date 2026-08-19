c74b222eadf0952c9e7ec8b165088be644cd3958
vinayak-rajput <vk.ra.org@gmail.com>
2026-08-19 09:35:33 +0530

    Fixed missing Constructor and Setter in Premium Calculator

--- src/com/hdfclife/strategy/PremiumCalculator.java
+++ src/com/hdfclife/strategy/PremiumCalculator.java
@@ -4,6 +4,16 @@ public class PremiumCalculator {
 
     PremiumStrategy premiumStrategy;
 
+    public PremiumCalculator(PremiumStrategy premiumStrategy) {
+
+        this.premiumStrategy = premiumStrategy;
+    }
+
+    public void setPremiumStrategy(PremiumStrategy premiumStrategy) {
+
+        this.premiumStrategy = premiumStrategy;
+    }
+
     double calculatePremium(PremiumStrategy premiumStrategy, double basePremium){
 
         return premiumStrategy.calculate(basePremium);
